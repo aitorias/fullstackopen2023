48e54c70e330192d86f98581ea06f59cbc71fde9
Aitorias <aitordediego23@gmail.com>
2023-07-03 14:46:42 +0200

    unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,25 +10,33 @@ function Button({ handleClick, text }) {
 
 function StatisticLine({ text, value }) {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 }
 
 function Statistics(props) {
-  if (props.all === 0) {
+  const all = props.good + props.neutral + props.bad || 0;
+  const average = (props.good - props.bad) / all || 0;
+  const positive = (props.good / all) * 100 || 0;
+
+  if (all === 0) {
     return <p>No feedback given</p>;
   }
+
   return (
-    <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.all} />
-      <StatisticLine text="all" value={props.average} />
-      <StatisticLine text="positive" value={props.positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="all" value={average} />
+        <StatisticLine text="positive" value={`${positive} %`} />
+      </tbody>
+    </table>
   );
 }
 
@@ -38,28 +46,17 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad || 0;
-  const average = (good - bad) / all || 0;
-  const positive = (good / all) * 100 || 0;
-
   return (
     <div>
       <Header title={"give feedback"} />
 
-      <Button handleClick={() => setGood(good + 1)} text={"good"} />
-      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
-      <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
       <Header title={"statistics"} />
 
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        all={all}
-        average={average}
-        positive={positive}
-      />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }

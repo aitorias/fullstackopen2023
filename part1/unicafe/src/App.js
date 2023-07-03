fb03fd51e49f0ac456aa54c54e814ddb40afc5be
Aitorias <aitordediego23@gmail.com>
2023-07-03 14:17:35 +0200

    unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+function Header({ title }) {
+  return <h1>{title}</h1>;
+}
+
 function Button({ handleClick, text }) {
   return <button onClick={handleClick}>{text}</button>;
 }
@@ -10,17 +14,24 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad || 0;
+  const average = (good - bad) / all || 0;
+  const positive = (good / all) * 100 || 0;
+
   return (
     <div>
-      <h1>give feedback</h1>
+      <Header title={"give feedback"} />
       <Button handleClick={() => setGood(good + 1)} text={"good"} />
       <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
       <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
 
-      <h2>statistics</h2>
+      <Header title={"statistics"} />
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive} %</p>
     </div>
   );
 }
